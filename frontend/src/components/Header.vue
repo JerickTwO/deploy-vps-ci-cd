afcd9d09587b33c3661143c69d7989e21ec9e14d
JerickTwO <jerickjjtm774@gmail.com>
2025-08-13 14:39:23 -0500

    Update header title from 'Schooll' to 'Test'

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,7 +4,7 @@
   >
     <RouterLink to="/">
       <div class="text-4xl font-semibold">
-        <span>Schooll</span>
+        <span>Test</span>
         <span class="text-yellow-500 italic">App</span>
       </div>
     </RouterLink>
